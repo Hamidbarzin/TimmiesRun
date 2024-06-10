2e896f3759c77fc2a8586bd5e014bca117a22837
Hamidbarzin <Hamidreza.Zebardastbarzin@triosstudent.com>
2024-06-10 09:05:34 -0400

    Initial Commit

---
+++ ChecklistItem.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistItem.swift
+//  TimmiesRun
+//
+//  Created by Hamidreza Zebardast on 2024-06-09.
+//
+
+import Foundation
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
